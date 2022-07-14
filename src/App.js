c9f3e89471302c998bcbf8025e38fb8424409ae0
LucasPCotrim <lucaspcotrim@gmail.com>
2022-07-14 00:01:20 -0300

    Implemented layout with single App React component

---
+++ src/App.js
@@ -0,0 +1,282 @@
+
+
+function App() {
+  return (
+    <>
+      <div className="top_menu">
+        <div>
+          <div className="top_menu_logo">
+            <ion-icon name="logo-instagram"></ion-icon>
+            <div className="linha_vertical"></div>
+            <img src="imgs/logo.png" alt="" width="103px" height="29px"/>
+          </div>
+          <div className="top_menu_searchbar">
+            <p>Pesquisar</p>
+          </div>
+          <div className="top_menu_icons">
+            <ion-icon name="paper-plane-outline"></ion-icon>
+            <ion-icon name="compass-outline"></ion-icon>
+            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name="person-outline"></ion-icon>
+          </div>
+        </div>
+      </div>
+      <div className="top_menu_mobile">
+        <ion-icon name="logo-instagram"></ion-icon>
+        <img src="imgs/logo.png" alt="" width="103px" height="29px"/>
+        <ion-icon name="paper-plane-outline"></ion-icon>
+      </div>
+      <div className="page_content">
+        <div className="main_page">
+          <div className="story_browser">
+            <div className="story">
+              <div className="story_icon">
+                <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px"/>
+                <img src="imgs/9gag 1.png" alt="" height="56px" width="56px"/>
+              </div>
+              <p>9gag</p>
+            </div>
+              <div className="story">
+                <div className="story_icon">
+                  <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px"/>
+                  <img src="imgs/story_meowed 1.png" alt="" height="56px" width="56px"/>
+                </div>
+                <p>meowed</p>
+              </div>
+              <div className="story">
+                <div className="story_icon">
+                  <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px"/>
+                  <img src="imgs/story_barked 1.png" alt="" height="56px" width="56px"/>
+                </div>
+                <p>barked</p>
+              </div>
+              <div className="story">
+                <div className="story_icon">
+                  <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px"/>
+                  <img src="imgs/story_nathanwpylestrangeplanet 1.png" alt="" height="56px" width="56px"/>
+                </div>
+                <p>nathanwpyle...</p>
+              </div>
+              <div className="story">
+                <div className="story_icon">
+                  <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px"/>
+                  <img src="imgs/story_wawawiwacomicsa 1.png" alt="" height="56px" width="56px"/>
+                </div>
+                <p>wawawiwa</p>
+              </div>
+              <div className="story">
+                <div className="story_icon">
+                  <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px"/>
+                  <img src="imgs/story_respondeai 1.png" alt="" height="56px" width="56px"/>
+                </div>
+                <p>respondeai</p>
+              </div>
+              <div className="story">
+                <div className="story_icon">
+                  <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px"/>
+                  <img src="imgs/story_filomoderna 1.png" alt="" height="56px" width="56px"/>
+                </div>
+                <p>filomoderna</p>
+              </div>
+              <div className="story">
+                <div className="story_icon">
+                  <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px"/>
+                  <img src="imgs/story_memeriagourmet 1.png" alt="" height="56px" width="56px"/>
+                </div>
+                <p>memeriago</p>
+              </div>
+              <div className="story_right_button">
+                <img src="imgs/chevron-forward-circle 1.png" alt="" width="32px" height="32px"/>
+              </div>   
+          </div>
+
+          <div className="posts_column">
+            <div className="post">
+              <div className="post_header">
+                <div>
+                  <div className="post_author">
+                    <img src="imgs/story_meowed 1.png" alt="" width="32px" height="32px"/>
+                    <p>meowed</p>
+                  </div>
+                  <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+              </div>
+              <img src="imgs/gato-telefone 1.png" alt=""/>
+              <div className="post_buttons">
+                <div>
+                  <div className="left_buttons">
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="chatbubble-outline"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                  </div>
+                  <div className="right_button">
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                  </div>
+                </div>
+              </div>
+              <div className="post_likes">
+                <img src="imgs/story_respondeai 1.png" alt="" width="20px" height="20px"/>
+                <p>Curtido por <b>respondeai</b> e <b>outras 101.523 pessoas</b></p>
+              </div>
+            </div>
+
+            <div className="post">
+              <div className="post_header">
+                <div>
+                  <div className="post_author">
+                    <img src="imgs/story_barked 1.png" alt="" width="32px" height="32px"/>
+                    <p>barked</p>
+                  </div>
+                  <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+              </div>
+              <img src="imgs/dog 1.png" alt=""/>
+              <div className="post_buttons">
+                <div>
+                  <div className="left_buttons">
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="chatbubble-outline"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                  </div>
+                  <div className="right_button">
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                  </div>
+                </div>
+              </div>
+              <div className="post_likes">
+                <img src="imgs/adorableanimals 2.png" alt="" width="20px" height="20px"/>
+                <p>Curtido por <b>adorable_animals</b> e <b>outras 99.159 pessoas</b></p>
+              </div>
+            </div>
+
+            <div className="post">
+              <div className="post_header">
+                <div>
+                    <div className="post_author">
+                        <img src="imgs/story_meowed 1.png" alt="" width="32px" height="32px"/>
+                        <p>meowed</p>
+                    </div>
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+              </div>
+              <img src="imgs/gato-telefone 1.png" alt=""/>
+              <div className="post_buttons">
+                <div>
+                  <div className="left_buttons">
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="chatbubble-outline"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                  </div>
+                  <div className="right_button">
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                  </div>
+                </div>
+              </div>
+              <div className="post_likes">
+                <img src="imgs/story_respondeai 1.png" alt="" width="20px" height="20px"/>
+                <p>Curtido por <b>respondeai</b> e <b>outras 101.523 pessoas</b></p>
+              </div>
+            </div>
+              
+          </div>
+        </div>
+          <div className="sidebar_container">
+            <div className="sidebar">
+              <div className="profile">
+                <img src="imgs/catanacomics 1.png" alt=""/>
+                <div className="profile_name">
+                  <b>catanacomics</b>
+                  <p>Catana</p>
+                </div>
+              </div>
+              <div className="suggestions">
+                <div className="suggestions_title">
+                  <div className="suggestions_title_left"><b>Sugestões para você</b></div>
+                  <div className="suggestions_title_right"><b>Ver tudo</b></div>
+                </div>
+                <div className="suggestion">
+                  <div className="suggested_profile">
+                    <img src="imgs/badvibesmemes 1.png" alt=""/>
+                    <div className="suggested_profile_name">
+                      <h1><b>bad.vibes.memes</b></h1>
+                      <p>Segue você</p>
+                    </div>
+                  </div>
+                  <div className="suggested_profile_action">
+                    <b>seguir</b>
+                  </div>
+                </div>
+                <div className="suggestion">
+                  <div className="suggested_profile">
+                    <img src="imgs/chibirdart 1.png" alt=""/>
+                    <div className="suggested_profile_name">
+                      <h1><b>chibirdart</b></h1>
+                      <p>Segue você</p>
+                    </div>
+                  </div>
+                  <div className="suggested_profile_action">
+                    <b>seguir</b>
+                  </div>
+                </div>
+                <div className="suggestion">
+                  <div className="suggested_profile">
+                    <img src="imgs/razoesparaacreditar 1.png" alt=""/>
+                    <div className="suggested_profile_name">
+                      <h1><b>razoes.para.acreditar</b></h1>
+                      <p>Novo no Instagram</p>
+                    </div>
+                  </div>
+                  <div className="suggested_profile_action">
+                    <b>seguir</b>
+                  </div>
+                </div>
+                <div className="suggestion">
+                  <div className="suggested_profile">
+                    <img src="imgs/adorableanimals 1.png" alt=""/>
+                    <div className="suggested_profile_name">
+                      <h1><b>adorable_animals</b></h1>
+                      <p>Segue você</p>
+                    </div>
+                  </div>
+                  <div className="suggested_profile_action">
+                    <b>seguir</b>
+                  </div>
+                </div>
+                <div className="suggestion">
+                  <div className="suggested_profile">
+                    <img src="imgs/smallcutecats 1.png" alt=""/>
+                    <div className="suggested_profile_name">
+                      <h1><b>smallcutecats</b></h1>
+                      <p>Segue você</p>
+                    </div>
+                  </div>
+                  <div className="suggested_profile_action">
+                    <b>seguir</b>
+                  </div>
+                </div>
+              </div>
+              <div className="sidebar_foot_menu">
+                <p>Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma</p>
+                <br/>
+                <p>© 2021 INSTAGRAM DO FACEBOOK</p>
+              </div>
+            </div>
+          </div>
+          
+      </div>
+      <div className="bottom_menu_mobile">
+        <div>
+          <ion-icon name="home"></ion-icon>
+          <ion-icon name="search-outline"></ion-icon>
+          <ion-icon name="add-circle-outline"></ion-icon>
+          <ion-icon name="heart-outline"></ion-icon>
+          <ion-icon name="person-outline"></ion-icon>
+        </div>
+      </div>
+  
+      <script script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+    </>
+  );
+}
+
+export default App;
