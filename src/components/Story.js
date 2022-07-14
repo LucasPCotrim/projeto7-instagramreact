65067d1a45a15558b43e67508c79d07c2e5816fd
LucasPCotrim <lucaspcotrim@gmail.com>
2022-07-14 01:30:09 -0300

    Created Story.js component with props and loaded Story components in StoryBrowser using map

---
+++ src/components/Story.js
@@ -0,0 +1,13 @@
+
+
+export default function Story(props){
+  return (
+    <div class="story">
+      <div class="story_icon">
+        <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+        <img src={props.img} alt={props.profile_name + ' story profile picture'} height="56px" width="56px" />
+      </div>
+      <p>{props.profile_name}</p>
+    </div>
+  );
+}
--- src/components/StoryBrowser.js
+++ src/components/StoryBrowser.js
@@ -1,67 +1,30 @@
+import Story from "./Story";
 
 
 export default function StoryBrowser(){
+  const stories = [{profile_name: '9gag',
+                    img: 'imgs/9gag 1.png'},
+                   {profile_name: 'meowed',
+                    img: 'imgs/story_meowed 1.png'},
+                   {profile_name: 'barked',
+                    img: 'imgs/story_barked 1.png'},
+                   {profile_name: 'nathanwpyle',
+                    img: 'imgs/story_nathanwpylestrangeplanet 1.png'},
+                   {profile_name: 'wawawiwa',
+                    img: 'imgs/story_wawawiwacomicsa 1.png'},
+                   {profile_name: 'respondeai',
+                    img: 'imgs/story_respondeai 1.png'},
+                   {profile_name: 'filomoderna',
+                    img: 'imgs/story_filomoderna 1.png'},
+                   {profile_name: 'memeriago',
+                    img: 'imgs/story_memeriagourmet 1.png'}]
+
   return (
     <div class="story_browser">
-      <div class="story">
-        <div class="story_icon">
-          <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
-          <img src="imgs/9gag 1.png" alt="" height="56px" width="56px" />
-        </div>
-        <p>9gag</p>
-      </div>
-        <div class="story">
-          <div class="story_icon">
-            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
-            <img src="imgs/story_meowed 1.png" alt="" height="56px" width="56px" />
-          </div>
-          <p>meowed</p>
-        </div>
-        <div class="story">
-          <div class="story_icon">
-            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
-            <img src="imgs/story_barked 1.png" alt="" height="56px" width="56px" />
-          </div>
-          <p>barked</p>
-        </div>
-        <div class="story">
-          <div class="story_icon">
-            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
-            <img src="imgs/story_nathanwpylestrangeplanet 1.png" alt="" height="56px" width="56px" />
-          </div>
-          <p>nathanwpyle...</p>
-        </div>
-        <div class="story">
-          <div class="story_icon">
-            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
-            <img src="imgs/story_wawawiwacomicsa 1.png" alt="" height="56px" width="56px" />
-          </div>
-          <p>wawawiwa</p>
-        </div>
-        <div class="story">
-          <div class="story_icon">
-            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
-            <img src="imgs/story_respondeai 1.png" alt="" height="56px" width="56px" />
-          </div>
-          <p>respondeai</p>
-        </div>
-        <div class="story">
-          <div class="story_icon">
-            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
-            <img src="imgs/story_filomoderna 1.png" alt="" height="56px" width="56px" />
-          </div>
-          <p>filomoderna</p>
-        </div>
-        <div class="story">
-          <div class="story_icon">
-            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
-            <img src="imgs/story_memeriagourmet 1.png" alt="" height="56px" width="56px" />
-          </div>
-          <p>memeriago</p>
-        </div>
-        <div class="story_right_button">
-          <img src="imgs/chevron-forward-circle 1.png" alt="" width="32px" height="32px" />
-        </div>   
+      {stories.map((story)=>{return <Story profile_name={story.profile_name} img={story.img}/>})}
+      <div class="story_right_button">
+        <img src="imgs/chevron-forward-circle 1.png" alt="" width="32px" height="32px" />
+      </div>   
     </div>
   );
 }
