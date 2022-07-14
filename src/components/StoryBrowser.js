53b052c26aa4a1776649ff65bc4dfb24122080ac
LucasPCotrim <lucaspcotrim@gmail.com>
2022-07-14 01:32:14 -0300

    refactoring of StoryBrowser.js

--- src/components/StoryBrowser.js
+++ src/components/StoryBrowser.js
@@ -4,18 +4,25 @@ import Story from "./Story";
 export default function StoryBrowser(){
   const stories = [{profile_name: '9gag',
                     img: 'imgs/9gag 1.png'},
+
                    {profile_name: 'meowed',
                     img: 'imgs/story_meowed 1.png'},
+
                    {profile_name: 'barked',
                     img: 'imgs/story_barked 1.png'},
+
                    {profile_name: 'nathanwpyle',
                     img: 'imgs/story_nathanwpylestrangeplanet 1.png'},
+
                    {profile_name: 'wawawiwa',
                     img: 'imgs/story_wawawiwacomicsa 1.png'},
+
                    {profile_name: 'respondeai',
                     img: 'imgs/story_respondeai 1.png'},
+
                    {profile_name: 'filomoderna',
                     img: 'imgs/story_filomoderna 1.png'},
+                    
                    {profile_name: 'memeriago',
                     img: 'imgs/story_memeriagourmet 1.png'}]
 
