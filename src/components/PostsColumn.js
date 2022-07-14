7a01eb2592f3a3c3e18a0f8bf4ab4ec7caab8824
LucasPCotrim <lucaspcotrim@gmail.com>
2022-07-14 01:09:15 -0300

    Componentization of mainPage.js

---
+++ src/components/PostsColumn.js
@@ -0,0 +1,95 @@
+
+
+export default function PostsColumn(){
+  return (
+    <div class="posts_column">
+      <div class="post">
+        <div class="post_header">
+          <div>
+            <div class="post_author">
+              <img src="imgs/story_meowed 1.png" alt="" width="32px" height="32px" />
+              <p>meowed</p>
+            </div>
+            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          </div>
+        </div>
+        <img src="imgs/gato-telefone 1.png" alt="" />
+        <div class="post_buttons">
+          <div>
+            <div class="left_buttons">
+              <ion-icon name="heart-outline"></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div class="right_button">
+              <ion-icon name="bookmark-outline"></ion-icon>
+            </div>
+          </div>
+        </div>
+        <div class="post_likes">
+          <img src="imgs/story_respondeai 1.png" alt="" width="20px" height="20px" />
+          <p>Curtido por <b>respondeai</b> e <b>outras 101.523 pessoas</b></p>
+        </div>
+      </div>
+
+      <div class="post">
+        <div class="post_header">
+          <div>
+            <div class="post_author">
+              <img src="imgs/story_barked 1.png" alt="" width="32px" height="32px" />
+              <p>barked</p>
+            </div>
+            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          </div>
+        </div>
+        <img src="imgs/dog 1.png" alt="" />
+        <div class="post_buttons">
+          <div>
+            <div class="left_buttons">
+              <ion-icon name="heart-outline"></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div class="right_button">
+              <ion-icon name="bookmark-outline"></ion-icon>
+            </div>
+          </div>
+        </div>
+        <div class="post_likes">
+          <img src="imgs/adorableanimals 2.png" alt="" width="20px" height="20px" />
+          <p>Curtido por <b>adorable_animals</b> e <b>outras 99.159 pessoas</b></p>
+        </div>
+      </div>
+
+      <div class="post">
+        <div class="post_header">
+          <div>
+              <div class="post_author">
+                  <img src="imgs/story_meowed 1.png" alt="" width="32px" height="32px" />
+                  <p>meowed</p>
+              </div>
+              <ion-icon name="ellipsis-horizontal"></ion-icon>
+          </div>
+        </div>
+        <img src="imgs/gato-telefone 1.png" alt="" />
+        <div class="post_buttons">
+          <div>
+            <div class="left_buttons">
+              <ion-icon name="heart-outline"></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div class="right_button">
+              <ion-icon name="bookmark-outline"></ion-icon>
+            </div>
+          </div>
+        </div>
+        <div class="post_likes">
+          <img src="imgs/story_respondeai 1.png" alt="" width="20px" height="20px" />
+          <p>Curtido por <b>respondeai</b> e <b>outras 101.523 pessoas</b></p>
+        </div>
+      </div>
+        
+    </div>
+  );
+}
---
+++ src/components/StoryBrowser.js
@@ -0,0 +1,67 @@
+
+
+export default function StoryBrowser(){
+  return (
+    <div class="story_browser">
+      <div class="story">
+        <div class="story_icon">
+          <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+          <img src="imgs/9gag 1.png" alt="" height="56px" width="56px" />
+        </div>
+        <p>9gag</p>
+      </div>
+        <div class="story">
+          <div class="story_icon">
+            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+            <img src="imgs/story_meowed 1.png" alt="" height="56px" width="56px" />
+          </div>
+          <p>meowed</p>
+        </div>
+        <div class="story">
+          <div class="story_icon">
+            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+            <img src="imgs/story_barked 1.png" alt="" height="56px" width="56px" />
+          </div>
+          <p>barked</p>
+        </div>
+        <div class="story">
+          <div class="story_icon">
+            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+            <img src="imgs/story_nathanwpylestrangeplanet 1.png" alt="" height="56px" width="56px" />
+          </div>
+          <p>nathanwpyle...</p>
+        </div>
+        <div class="story">
+          <div class="story_icon">
+            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+            <img src="imgs/story_wawawiwacomicsa 1.png" alt="" height="56px" width="56px" />
+          </div>
+          <p>wawawiwa</p>
+        </div>
+        <div class="story">
+          <div class="story_icon">
+            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+            <img src="imgs/story_respondeai 1.png" alt="" height="56px" width="56px" />
+          </div>
+          <p>respondeai</p>
+        </div>
+        <div class="story">
+          <div class="story_icon">
+            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+            <img src="imgs/story_filomoderna 1.png" alt="" height="56px" width="56px" />
+          </div>
+          <p>filomoderna</p>
+        </div>
+        <div class="story">
+          <div class="story_icon">
+            <img src="imgs/stories_background.jpg" alt="" height="66px" width="66px" />
+            <img src="imgs/story_memeriagourmet 1.png" alt="" height="56px" width="56px" />
+          </div>
+          <p>memeriago</p>
+        </div>
+        <div class="story_right_button">
+          <img src="imgs/chevron-forward-circle 1.png" alt="" width="32px" height="32px" />
+        </div>   
+    </div>
+  );
+}
